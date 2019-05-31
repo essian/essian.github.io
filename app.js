978e65c6871d07900dfbcbfeac173e1d93186ee9
Jessica Dennis <jessicampdennis@hotmail.com>
2019-05-31 14:42:20 +0100

    add metric calculator

--- app.js
+++ app.js
@@ -1,34 +1,109 @@
-document.getElementById('grist-form').addEventListener('submit', function (e) {
+document.getElementById('grist-form-metric').style.display = 'none'
+
+// Toggle mixed and metric
+
+document.getElementById('metric-button').addEventListener('click', function(e){
+    reset()
+    document.getElementById('grist-form-mixed').style.display = "none"
+    document.getElementById('grist-form-metric').style.display = 'block'
+
+})
+
+document.getElementById('mixed-button').addEventListener('click', function(e){
+    reset()
+    document.getElementById('grist-form-mixed').style.display = "block"
+    document.getElementById('grist-form-metric').style.display = 'none'
+
+})
+
+// Calculate Mixed Result
+
+document.getElementById('grist-form-mixed').addEventListener('submit', function (e) {
     document.getElementById('results').style.display = 'none'
     document.getElementById('loading').style.display = 'block'
-    setTimeout(calculateResults, 1000)
+    setTimeout(calculateResultsMixed, 1000)
     e.preventDefault()
 })
+
+// Calculate Metric Result
+
+document.getElementById('grist-form-metric').addEventListener('submit', function (e) {
+    document.getElementById('results').style.display = 'none'
+    document.getElementById('loading').style.display = 'block'
+    setTimeout(calculateResultsMetric, 1000)
+    e.preventDefault()
+})
+
+
+// Reset both forms
 
 document.getElementById('reset-button').addEventListener('click', reset) 
 
 function reset() {
-        document.getElementById('grist-form').reset()
-        document.getElementById('results').style.display = 'none'
+    document.getElementById('grist-form-mixed').reset()
+    document.getElementById('grist-form-metric').reset()
+    document.getElementById('results').style.display = 'none'
 }
 
-function calculateResults() {
-    const weightInGrams = parseFloat(document.getElementById('weight').value)
-    const length = parseFloat(document.getElementById('length').value)
-    const wraps = parseFloat(document.getElementById('wraps').value)
+// Mixed Calculation
+
+function calculateResultsMixed() {
+    const weightInGrams = parseFloat(document.getElementById('mixed-weight-g').value)
+    const length = parseFloat(document.getElementById('mixed-length-inches').value)
+    const wraps = parseFloat(document.getElementById('mixed-wraps').value)
     const gristMetricField = document.getElementById('gristMetric')
     const gristImperialField = document.getElementById('gristImperial')
     const yarnClass = document.getElementById('class')
     const totalLength = document.getElementById('totalLengthYards')
+    const totalLengthMeters = document.getElementById('totalLengthMeters')
 
     const totalLengthInYards = (wraps * 2 * length) / 36
     const totalLengthInMeters = totalLengthInYards / 1.094
     const weightInPounds = weightInGrams / 453.592
     const gristMetric = totalLengthInMeters / (weightInGrams / 1000)
     const gristImperial = totalLengthInYards / weightInPounds
+    console.log(wraps)
+    console.log(totalLengthInMeters)
+    console.log(gristImperial)
 
     if (isFinite(gristImperial)) {
         totalLengthYards.value = totalLengthInYards.toFixed(2)
+        totalLengthMeters.value = totalLengthInMeters.toFixed(2)
+        gristMetricField.value = gristMetric.toFixed(2)
+        gristImperialField.value = gristImperial.toFixed(2)
+        yarnClass.value = getYarnClass(gristImperial)
+        document.getElementById('loading').style.display = 'none'
+        document.getElementById('results').style.display = 'block'
+    } else {
+        showError('Please check your inputs')
+        document.getElementById('loading').style.display = 'none'
+        document.getElementById('results').style.display = 'none'
+    }
+
+}
+
+// Metric Calculation
+
+function calculateResultsMetric() {
+    const weightInGrams = parseFloat(document.getElementById('metric-weight-g').value)
+    const length = parseFloat(document.getElementById('metric-length-cm').value)
+    const wraps = parseFloat(document.getElementById('metric-wraps').value)
+    const gristMetricField = document.getElementById('gristMetric')
+    const gristImperialField = document.getElementById('gristImperial')
+    const yarnClass = document.getElementById('class')
+    const totalLength = document.getElementById('totalLengthYards')
+    const totalLengthMeters = document.getElementById('totalLengthMeters')
+
+    const totalLengthInMeters = length * wraps * 2 / 100
+    const totalLengthInYards = totalLengthInMeters * 1.094
+    
+    const weightInPounds = weightInGrams / 453.592
+    const gristMetric = totalLengthInMeters / (weightInGrams / 1000)
+    const gristImperial = totalLengthInYards / weightInPounds
+
+    if (isFinite(gristImperial)) {
+        totalLengthYards.value = totalLengthInYards.toFixed(2)
+        totalLengthMeters.value = totalLengthInMeters.toFixed(2)
         gristMetricField.value = gristMetric.toFixed(2)
         gristImperialField.value = gristImperial.toFixed(2)
         yarnClass.value = getYarnClass(gristImperial)
