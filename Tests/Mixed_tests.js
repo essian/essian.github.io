f2e2d388c48ac9f0213a33ef68826aa8ea8cf9e0
Jessica Dennis <jessicampdennis@hotmail.com>
2019-06-06 17:12:59 +0100

    add config to allow debugging tests in vs code and complete first test

--- Tests/Mixed_tests.js
+++ Tests/Mixed_tests.js
@@ -4,13 +4,14 @@ import {
 
 fixture `Grist Calculator`.page `http://127.0.0.1:5500/index.html?#`;
 
-test(`Calculation is correct`, async t => {
+test(`Ypp calculation is correct`, async t => {
             await t
                 .typeText('#mixed-weight-g', '190')
                 .typeText('#mixed-wraps', '445')
                 .typeText('#mixed-length-inches', '18')
                 .click('#calculate-mixed')
 
-                let ypp = await Selector('#gristImperial')
-                await t.expect(ypp.innertext).eql('1')
+                const ypp = await Selector('#gristImperial').with({visibilityCheck: true})()
+                
+                await t.expect(ypp.value).eql('1062.36')
                 })
